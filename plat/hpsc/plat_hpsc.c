2e16e7cb4ade5fdcf4530ca89c101b367c7f3306
Alexei Colin <acolin@isi.edu>
2018-11-09 18:01:59 -0500

    hpsc: update format strings to changed types

--- plat/hpsc/plat_hpsc.c
+++ plat/hpsc/plat_hpsc.c
@@ -12,13 +12,15 @@ int plat_core_pos_by_mpidr(u_register_t mpidr)
 	WARN("plat_core_pos_by_mpidr: start with mpidr(0x%lx)\n", mpidr);
 	if (((mpidr & MPIDR_CLUSTER_MASK) != 0) && ((mpidr & MPIDR_CLUSTER_MASK) != 0x100)) 
 	{
-		WARN("plat_core_pos_by_mpidr: mpidr - 0x%lx, MPIDR_CUSTER_MASK - 0x%x, mpidr & MPIDR_CLUSTER_MASK = 0x%lx\n", mpidr, MPIDR_CLUSTER_MASK, mpidr & MPIDR_CLUSTER_MASK);
+		WARN("plat_core_pos_by_mpidr: mpidr - 0x%lx, MPIDR_CUSTER_MASK - 0x%llx, mpidr & MPIDR_CLUSTER_MASK = 0x%llx\n",
+                     mpidr, MPIDR_CLUSTER_MASK, mpidr & MPIDR_CLUSTER_MASK);
 		return -1;
 	}
 
 	if ((mpidr & MPIDR_CPU_MASK) >= PLATFORM_CORE_COUNT)
 	{
-		WARN("plat_core_pos_by_mpidr: >= PLATFORM_CORE_COUNT : mpidr - 0x%lx, MPIDR_CPU_MASK - 0x%x, mpidr & MPIDR_CPU_MASK = 0x%lx\n", mpidr, MPIDR_CPU_MASK, mpidr & MPIDR_CPU_MASK);
+		WARN("plat_core_pos_by_mpidr: >= PLATFORM_CORE_COUNT : mpidr - 0x%lx, MPIDR_CPU_MASK - 0x%llx, mpidr & MPIDR_CPU_MASK = 0x%llx\n",
+                     mpidr, MPIDR_CPU_MASK, mpidr & MPIDR_CPU_MASK);
 		return -1;
 	}
 
