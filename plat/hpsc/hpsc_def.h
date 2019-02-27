afd6d3994d2a0849d5017d87f663654568847e30
Alexei Colin <acolin@isi.edu>
2019-02-27 12:22:17 -0500

    hpsc all: uart baudrate to 500000 bps
    The clock in Zebu (and potential in real HW) is 16Mhz. Switch
    to a baudrate that can be generated precisely by this clock:
    16Mhz / (16 * 500000) = 2. Note: factor of 16 comes from Synopsys
    DWC UART datasheet.

--- plat/hpsc/hpsc_def.h
+++ plat/hpsc/hpsc_def.h
@@ -167,7 +167,7 @@
 #define HPSC_UART_CLOCK         16000000
 
 /* Must be non zero */
-#define HPSC_UART_BAUDRATE	115200
+#define HPSC_UART_BAUDRATE	500000
 #define ARM_CONSOLE_BAUDRATE	HPSC_UART_BAUDRATE
 
 /* Silicon version detection */
